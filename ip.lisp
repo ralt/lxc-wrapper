86c69c8a58aeec5fb927ff7851507e87db40705a
Florian MARGAINE <florian@margaine.com>
2015-01-29 15:45:35 +0100

    Generates the correct IP

--- ip.lisp
+++ ip.lisp
@@ -1,34 +1,38 @@
 (in-package #:lxc-wrapper)
 
 (defvar *hosts-file* #p"/etc/hosts")
-;;; IP in list form, appended with the mask
-(defvar *lxc-network* '(10 0 3 0 24))
+(defvar *lxc-network* '(10 0 3 0))
 (defvar *ip-regex* "(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)")
 
 (defun next-ip ()
   "Finds the next available IP for the LXC"
-  (generate-next-ip
-   (with-open-file (file *hosts-file*)
-     (loop for line = (read-line file nil)
-	while line
-	when (line-matches-ip line)
-	collect (line-get-ip line)))))
+  (format
+   nil
+   "~{~D~^.~}"
+   (generate-next-ip
+    (with-open-file (file *hosts-file*)
+      (loop for line = (read-line file nil)
+	 while line
+	 when (line-matches-ip line)
+	 collect (line-get-ip line))))))
 
 (defun add-ip (file ip extension)
   "Adds the ip:extension pair to the hosts file"
   ;; @todo
   )
 
-(defun line-matches-p (line)
+(defun line-matches-ip (line)
   "Finds if the line matches an IP. Which means that
 the line must starts with an IP address."
   (multiple-value-bind (match numbers)
       (cl-ppcre:scan-to-strings *ip-regex* (car (cl-ppcre:split " " line)))
     (declare (ignore match))
+    (unless numbers
+      (return-from line-matches-ip))
     (let ((int-numbers (mapcar #'parse-integer (vector-to-list numbers))))
-      (loop for i from 0 upto (- (round (/ (elt *lxc-network* 4) 8)) 1)
-	 when (/= (elt *lxc-network* i) (elt int-numbers i))
-	 do (return-from line-matches-p nil))
+      (loop for i from 0 upto 2 ;; only /24 supported
+	 unless (= (elt *lxc-network* i) (elt int-numbers i))
+	 do (return-from line-matches-ip nil))
       t)))
 
 (defun vector-to-list (vector)
@@ -36,9 +40,28 @@ the line must starts with an IP address."
 
 (defun line-get-ip (line)
   "Gets the IP of a line."
-  (cl-ppcre:scan-to-strings *ip-regex* (car (cl-ppcre:split " " line))))
+  (multiple-value-bind (matches vector)
+      (cl-ppcre:scan-to-strings *ip-regex* (car (cl-ppcre:split " " line)))
+    (declare (ignore matches))
+    (mapcar #'parse-integer (vector-to-list vector))))
 
 (defun generate-next-ip (ips)
   "Generates the next IP from a list of IPs."
-  ;; @todo
-  )
+  ;; Just try to make sequential IPs using the netmask until one
+  ;; doesn't exist.
+  ;; We can't just sort the list of IPs and add one, because the IPs
+  ;; may not be sequential.
+  (let ((numbers (loop for i in ips collect (elt i 3))))
+    (loop for i from 2 upto 254
+       unless (member i numbers)
+       do (return-from generate-next-ip (new-ip *lxc-network* i 3)))))
+
+(defun new-ip (ip number place)
+  "Gets a new IP from a full IP and its last number."
+  (let ((counter -1))
+    (loop for i in ip
+       collect (progn
+		 (incf counter)
+		 (if (= counter place)
+		     number
+		     i)))))
